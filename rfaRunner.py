1a04260f43a6de6309d95ac212437d26c107d6d7
Ivan Korolevskiy <ivan.korolevskiy@emc.com>
2016-10-23 02:34:52 -0700

    assignment_01

---
+++ rfaRunner.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 19, 2016
+
+@author: sashaalexander
+'''
+from rfaUtils import getLog,qaprint
+
+log = getLog()
+
+message = "It is working, right?"
+qaprint(log,message)
+
---
+++ rfaUtils.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 19, 2016
+
+@author: sashaalexander
+'''
+
+def getLog():
+    
+    return 1
+
+
+def qaprint():
