447f425adf636d68ad7f09d7c9eecd4e5823d2a0
Nixer <nixer1978@yahoo.com>
2016-10-23 15:15:11 -0700

    assignment#01

--- rfaUtils.py
+++ rfaUtils.py
@@ -1,12 +1,35 @@
 '''
-Created on Oct 19, 2016
+Created on Oct 20, 2016
 
-@author: sashaalexander
+@author: nixer (team#2)
 '''
+import os, datetime
+
+def getCurTime():
+    #get today time and format it
+    today = datetime.datetime.now()
+    time_stamp = today.strftime('%Y%m%d_%H:%M')
+    return time_stamp
 
 def getLog():
+    try:        
+        #variables for directory path, time stamp and file name
+        dir_path = os.path.dirname(os.path.realpath(__file__))+'/logs/'
+        time_stamp = getCurTime()
+        file_name = dir_path + "testrun_" + time_stamp + ".log"
+        
+        #if logs directory does not exist -> create it
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
     
-    return 1
+        file = open(file_name, 'a') #create and open file in append mode
+        return file
+    
+    except OSError as er:
+        print er
+        return -1
 
-
-def qaprint():
+def qaprint(log, string):
+    print string #pass string to screen
+    log.write(string + '\n') #pass string to log file
+    #log.close()
