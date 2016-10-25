ad4ed09f3849b21c9f37344d79f66152b0989e9a
stepikogs <stepikogs@gmail.com>
2016-10-25 11:38:27 -0700

    no updates but console commit

--- rfaUtils.py
+++ rfaUtils.py
@@ -2,11 +2,24 @@
 Created on Oct 19, 2016
 
 @author: sashaalexander
+@author: georgy.stepiko
 '''
+import datetime
 
+# format is testrun_20161020_18:43.log
 def getLog():
+    try:
+        filename = 'logs/testrun_' + getCurTime() + '.log'
+        log = open(filename, 'a')
+        return log
+    except OSError as er:
+        print er
+        return -1
+
+def qaPrint(log, line):
+    log.write(getCurTime() + ' :: ' + line + '\r\n')#Don't forget about timestamps in logfiles.
+    print(line)#finally display text - no timestamp here
     
-    return 1
-
-
-def qaprint():
+def getCurTime():
+    timestamp = datetime.datetime.now().strftime('%Y%m%d_%H:%M') #time format is 20161020_18:43  
+    return  timestamp
