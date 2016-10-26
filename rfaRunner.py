c5673db602058bd5b7b96df3e652b2c0a2430e64
stepikogs <stepikogs@gmail.com>
2016-10-25 18:00:29 -0700

    good filepath flow + some silent errcheck attempts

--- rfaRunner.py
+++ rfaRunner.py
@@ -4,9 +4,9 @@ Created on Oct 19, 2016
 @author: sashaalexander
 @author: georgy.stepiko
 '''
-from rfaUtils import getLog,qaPrint
+from rfaUtils import getLog,qaPrint, dropLog  # @UnresolvedImport
 
 log = getLog()
 message = "It is working, right?"
 qaPrint(log,message)
-#something to close file here
+dropLog(log)
--- rfaUtils.py
+++ rfaUtils.py
@@ -5,19 +5,37 @@ Created on Oct 19, 2016
 @author: georgy.stepiko
 '''
 import datetime
+import os
 
 # format is testrun_20161020_18:43.log
 def getLog():
     try:
-        filename = 'logs/testrun_' + getCurTime() + '.log'
+        #filename = 'logs/testrun_' + getCurTime() + '.log'
+        filepath = os.path.dirname(os.path.realpath(__file__)) +'/logs'
+        filename = filepath + '/testrun_' + getCurTime() + '.log'
+        if not os.path.isdir(filepath):
+            try:
+                os.makedirs(filepath)
+            except OSError as er:
+                print er
+                return -1
+        print filename
         log = open(filename, 'a')
         return log
     except OSError as er:
         print er
         return -1
+    
+def dropLog(log):
+    try:
+        log.close()
+    except OSError as er:
+        print er
+        return -1
 
 def qaPrint(log, line):
-    log.write(getCurTime() + ' :: ' + line + '\r\n')#Don't forget about timestamps in logfiles.
+    timestamp = getCurTime() 
+    log.write( timestamp + ' :: ' + line + '\r\n')#Don't forget about timestamps in logfiles.
     print(line)#finally display text - no timestamp here
     
 def getCurTime():
