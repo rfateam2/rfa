e025bd2e878be1bf31c7dff2eafebc5729e59429
anya <anya.dyshleva@qolsys.com>
2016-10-25 17:55:52 -0700

    1st assignment

--- rfaRunner.py
+++ rfaRunner.py
@@ -3,10 +3,10 @@ Created on Oct 19, 2016
 
 @author: sashaalexander
 '''
+
 from rfaUtils import getLog,qaprint
 
 log = getLog()
-
 message = "It is working, right?"
-qaprint(log,message)
 
+qaprint(log,message)
--- rfaUtils.py
+++ rfaUtils.py
@@ -3,10 +3,27 @@ Created on Oct 19, 2016
 
 @author: sashaalexander
 '''
+import os
+import datetime
+
+def getCurTime():
+    time = datetime.datetime.now()
+    timeNow = time.strftime('_%Y%m%d_%H:%M')
+    return timeNow
 
 def getLog():
-    
-    return 1
+    try:
+        path = os.path.dirname(os.path.realpath(__file__))+'/logs/'
+        show_time = getCurTime()
+        log_name = path + "testrun_" + show_time + ".log"
+        if not os.path.exists(path):
+            os.makedirs(path)
+        log = open(log_name, 'a')
+        return log
+    except OSError as er:
+        return -1
 
-
-def qaprint():
+def qaprint(log, string):    
+    print string
+    log.write(string + '\n')
+    log.close()
