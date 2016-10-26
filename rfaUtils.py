c4cc7aabdac6d751750c1126dae0294cd63fabfe
Nixer <nixer1978@yahoo.com>
2016-10-26 09:04:01 -0700

    pre-merge fixes

--- rfaUtils.py
+++ rfaUtils.py
@@ -1,12 +1,13 @@
 '''
 Created on Oct 20, 2016
+
 @author: nixer (team#2)
 '''
 import os
 import datetime
 import sys
 
-def getCurTime():
+def get_cur_time():
     #get today time and format it
     today = datetime.datetime.now()
     time_stamp = today.strftime('%Y%m%d_%H:%M')
@@ -17,18 +18,17 @@ def create_dir(dir_path):
     if not os.path.exists(dir_path):
         try:    
             os.makedirs(dir_path)
-        except Exception, er:
-            print >> sys.stderr, "Unable to create 'logs' directory"
-            print >> sys.stderr, "Exception: %s" % str(er)
+        except OSError as er:
+            print er
             sys.exit(1)
 
-def getLog():
+def get_log():
     try:
         #variables for directory path, time stamp and file name
         dir_path = os.path.dirname(os.path.realpath(__file__))+'/logs/'
-        time_stamp = getCurTime()
-        file_name = dir_path + "testrun_" + time_stamp + ".log"
         create_dir(dir_path)
+        time_stamp = get_cur_time()
+        file_name = dir_path + "testrun_" + time_stamp + ".log"
         log = open(file_name, 'a') #create and open log in append mode
         return log
 
@@ -36,9 +36,15 @@ def getLog():
         print er
         return -1
 
-def qaprint(log, string):
+def qa_print(log, string):
     print string #pass string to screen
     log.write(string + '\n') #pass string to log file
 
 def close_log(log):
-    log.close()
+    #if log file exists -> close it
+    if os.path.isfile(log.name):
+        try:
+            log.close()
+        except OSError as er:
+            print er
+            sys.exit(1)
