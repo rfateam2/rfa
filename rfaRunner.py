30139761e08f48defe7052ac562802aec39b8f6c
RFA Team 2 <korvinca@gmail.com>
2016-10-26 21:01:32 -0700

    Update rfaRunner.py

--- rfaRunner.py
+++ rfaRunner.py
@@ -8,17 +8,10 @@ Created on Oct 20, 2016
 from rfaUtils import get_log, qa_print, close_log
 
 
-def printlog():
-    """ Save message in the log file and print in the console
-    """
-    log = get_log()
-    # (log_file mode, log_file path)
-    message = "It is working, right?"
-    loghd = log[0]
-    qa_print(loghd, message)
-    logpath = log[1]
-    close_log(logpath)
-
-
-if __name__ == '__main__':
-    printlog()
+log = get_log()
+# (log_file mode, log_file path)
+message = "It is working, right?"
+loghd = log[0]
+qa_print(loghd, message)
+logpath = log[1]
+close_log(logpath)
