e491343cd9180baed34fa19c813ff7e4a2c7352b
RFA Team 2 <korvinca@gmail.com>
2016-10-26 20:43:39 -0700

    Update rfaRunner.py

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,13 +1,24 @@
 '''
-Created on Oct 19, 2016
+RAF
+Created on Oct 20, 2016
 
-@author: sashaalexander
+@author: team#2
 '''
+
 from rfaUtils import get_log, qa_print, close_log
 
-log = get_log()
-# (log_file mode, log_file path)
-message = "It is working, right?"
-loghd = log[0]
-qa_print(loghd, message)
-close_log(log[1])
+
+def printlog():
+    """ Save message in the log file and print in the console
+    """
+    log = get_log()
+    # (log_file mode, log_file path)
+    message = "It is working, right?"
+    loghd = log[0]
+    qa_print(loghd, message)
+    logpath = log[1]
+    close_log(logpath)
+
+
+if __name__ == '__main__':
+    printlog()
