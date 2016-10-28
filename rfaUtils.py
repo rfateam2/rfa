c14873531bf130d40c7b1ef8bf0d9e0d7bbc1a03
Nixer <nixer1978@yahoo.com>
2016-10-28 10:03:54 -0700

    assignment_02

--- rfaUtils.py
+++ rfaUtils.py
@@ -1,50 +1,50 @@
 '''
-Created on Oct 20, 2016
+Created on Oct 19, 2016
 
-@author: nixer (team#2)
+@author: sashaalexander
+@author: team X
 '''
+from datetime import datetime
 import os
-import datetime
-import sys
 
-def get_cur_time():
-    #get today time and format it
-    today = datetime.datetime.now()
-    time_stamp = today.strftime('%Y%m%d_%H:%M')
-    return time_stamp
 
-def create_dir(dir_path):
-    #if logs directory does not exist -> create it
-    if not os.path.exists(dir_path):
-        try:    
-            os.makedirs(dir_path)
-        except OSError as er:
-            print er
-            sys.exit(1)
+def getLog():
+    """
+    Creates 'logs' directory, if it doesn't exist,
+    creates or opens a log file in 'logs' directory.
+    """
+    # assign a current working directory + '/logs' to log_dir variable (platform independent)
+    log_dir = os.path.join(os.getcwd(), "logs")
+    # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+    # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), "logs")
 
-def get_log():
     try:
-        #variables for directory path, time stamp and file name
-        dir_path = os.path.dirname(os.path.realpath(__file__))+'/logs/'
-        create_dir(dir_path)
-        time_stamp = get_cur_time()
-        file_name = dir_path + "testrun_" + time_stamp + ".log"
-        log = open(file_name, 'a') #create and open log in append mode
+        # if logs directory(!) doesn't exist, create it
+        if not os.path.isdir(log_dir):
+            os.makedirs(log_dir)
+        # open log file with prefix and timestamp (platform independent) in Append mode
+        log = open(os.path.join(log_dir, "rfaRunner_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
         return log
-
-    except OSError as er:
-        print er
+    except (OSError, IOError):
+        # return -1 in case of exception
         return -1
 
-def qa_print(log, string):
-    print string #pass string to screen
-    log.write(string + '\n') #pass string to log file
 
-def close_log(log):
-    #if log file exists -> close it
-    if os.path.isfile(log.name):
-        try:
-            log.close()
-        except OSError as er:
-            print er
-            sys.exit(1)
+def qaPrint(log, message):
+    """
+    Prints 'timestamp + message' to console and writes it to the log file
+    """
+    # current date and time as string + message. example: [Oct 25 01:52:33.000001] TC1 - Passed
+    log_message = getCurTime("[%b %d %H:%M:%S.%f]") + " " + message
+    # prints log_message
+    print log_message
+    # writes message to a log file
+    log.write(log_message + "\n")
+
+
+def getCurTime(date_time_format):
+    """
+    Returns current date_time as a string formatted according to date_time_format
+    """
+    date_time = datetime.now().strftime(date_time_format)
+    return date_time
