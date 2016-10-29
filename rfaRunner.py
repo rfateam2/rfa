14cc6ef774b4c728297c1960561dc4a3030e9d16
Nixer <nixer1978@yahoo.com>
2016-10-28 21:46:13 -0700

    getTestCases and getLocalEnv funcs added

--- rfaRunner.py
+++ rfaRunner.py
@@ -5,12 +5,24 @@ Created on Oct 19, 2016
 @author: sashaalexander
 @author: team 2
 '''
-from rfaUtils import getLog,qaPrint
-
+from rfaUtils import getLog, qaPrint, getLocalEnv, getTestCases
 import sys
 
+# processing of command-line arguments
+args = sys.argv[1].lower().split('=')
+if args[0] == '--testrun':
+    trid = args[1]
+else:
+    sys.exit('rfaRunner.py --testrun=<trid>')  
+
+# get list of test_cases
+test_cases = getTestCases(trid)
+
+loc_prop = getLocalEnv('local.properties')
+log_dir = loc_prop['log_dir']
+
 # get the log file handle
-log = getLog()
+log = getLog(log_dir, trid)
 
 # exit if log creation failed
 if log == -1:
--- rfaUtils.py
+++ rfaUtils.py
@@ -7,14 +7,13 @@ Created on Oct 19, 2016
 from datetime import datetime
 import os
 
-
-def getLog():
+def getLog(logdir, trid):
     """
     Creates 'logs' directory, if it doesn't exist,
     creates or opens a log file in 'logs' directory.
     """
     # assign a current working directory + '/logs' to log_dir variable (platform independent)
-    log_dir = os.path.join(os.getcwd(), "logs")
+    log_dir = os.path.join(os.getcwd(), logdir)
     # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
     # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), "logs")
 
@@ -23,7 +22,7 @@ def getLog():
         if not os.path.isdir(log_dir):
             os.makedirs(log_dir)
         # open log file with prefix and timestamp (platform independent) in Append mode
-        log = open(os.path.join(log_dir, "rfaRunner_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
+        log = open(os.path.join(log_dir, "rfaRunner_" + trid + "_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
         return log
     except (OSError, IOError):
         # return -1 in case of exception
@@ -48,3 +47,34 @@ def getCurTime(date_time_format):
     """
     date_time = datetime.now().strftime(date_time_format)
     return date_time
+
+
+def getLocalEnv(loc_prop_file):
+    '''
+    Reads the content of loc_prop_file to dictionary and returns it 
+    '''
+    loc_prop = {}
+    try:
+        with open(loc_prop_file) as f:
+            for line in f:
+               (key, val) = line.split('=')
+               loc_prop[key] = val.strip()
+        return loc_prop
+    except (OSError, IOError):
+        # return -1 in case of exception
+        return -1
+
+def getTestCases(trid):    
+    keys = ('tcid', 'rest_URL', 'HTTP_method', 'HTTP_RC_desired', 'param_list')
+    test_cases = []
+    try:
+        with open(trid+".txt") as f:
+            tc_list = f.readlines()
+        for i in range(0, len(tc_list)):
+            tc = tc_list[i].split("|")                    
+            dictionary = dict(zip(keys, map(str.strip, tc)))
+            test_cases.append(dictionary)      
+        return test_cases
+    except (OSError, IOError):
+        # return -1 in case of exception
+        return -1
