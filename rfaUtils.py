d61efe5dde892871c3eed448564a8397084702a5
Ivan Korolevskiy <ivan.korolevskiy@emc.com>
2016-10-30 16:02:27 -0700

    getTestCases v1

--- rfaUtils.py
+++ rfaUtils.py
@@ -7,7 +7,47 @@ Created on Oct 19, 2016
 '''
 from datetime import datetime
 import os
+import sys
 
+
+def getTestCases(trid=None):
+    """
+    Function will read corresponding id.txt file.
+    Function will return that dictionary or -1 in case of error
+    and will try to explain the reason
+    """
+    fname = "42.txt"
+    try:
+        handle = open(fname)
+    except:
+        raise
+        return -1
+    req_keys = ["rest_URL", "HTTP_method", "HTTP_RC_desired", "param_list"]
+    res_suite = dict()
+    for line in handle:
+        line = line.rstrip()
+        if len(line) < 1 : continue
+        req_values = line.split("|")
+        test_num = int(req_values[0])
+        req_values = req_values[1:]
+        one_test = dict(zip(req_keys, req_values))
+        res_suite[test_num] = one_test
+    if len(res_suite) < 1:
+        res_suite = -1
+    print res_suite
+    return res_suite
+
+
+    #lst.append(])
+#print lst
+
+def getLocalEnv(prop_file):
+    """
+    Read the content of the file, and return a dictionary,
+    where 1 value of the file becomes key and second value becomes a value.
+    """
+    local_prop = ''
+    return local_prop
 
 def getLog():
     """
@@ -49,3 +89,6 @@ def getCurTime(date_time_format):
     """
     date_time = datetime.now().strftime(date_time_format)
     return date_time
+
+if __name__ == '__main__':
+    getTestCases()
