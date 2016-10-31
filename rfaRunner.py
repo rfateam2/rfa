5dc86f4f59ff78506f17caa781c620b0fb3eb2d6
sana <oshporlyuk@gmail.com>
2016-10-30 20:20:09 -0700

    Assignment 02

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,11 +1,35 @@
 '''
 Created on Oct 19, 2016
 
+
 @author: sashaalexander
+@author: team X
 '''
-from rfaUtils import getLog,qaprint
+from rfaUtils import getLog,qaPrint,getTestCases
+import sys
 
+trid = -1
+for arg in sys.argv:    
+    if arg.lower().find("--testrun=") > -1:
+        trid = int(arg.split("=")[1])
+        
+test_cases = getTestCases(trid)
+if test_cases == -1:
+    sys.exit("could not read test run file")
+
+# get the log file handle
 log = getLog()
-
-message = 'It is working, right?'
-qaprint(log,message)
+ 
+# exit if log creation failed
+if log == -1:
+    sys.exit("Unable to create log file")
+ 
+message = "It is working, right?"
+ 
+# call qaPrint to print a message with timestamp and write it to the log file
+qaPrint(log, message)
+qaPrint(log, "Me like what me see")
+ 
+# close the log file if it open
+if not log.closed:
+    log.close()
--- rfaUtils.py
+++ rfaUtils.py
@@ -2,27 +2,77 @@
 Created on Oct 19, 2016
 
 @author: sashaalexander
+@author: team X
 '''
-import datetime
+from datetime import datetime
 import os
 
 
 def getLog():
-    directory = '../log'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    """
+    Creates 'logs' directory, if it doesn't exist,
+    creates or opens a log file in 'logs' directory.
+    """
+    # assign a current working directory + '/logs' to log_dir variable (platform independent)
+    #log_dir = os.path.join(os.getcwd(), "logs")
+    prop_set = getLocalEnv("local.properties")
+    log_dir = prop_set["log_dir"]
     
-    file_name = 'testrun_' + getCurTime().strftime('%Y%m%d_%H:%M') + '.log'
-    full_path = os.path.join(directory, file_name)
+    # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+    # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), "logs")
+
+    try:
+        # if logs directory(!) doesn't exist, create it
+        if not os.path.isdir(log_dir):
+            os.makedirs(log_dir)
+        # open log file with prefix and timestamp (platform independent) in Append mode
+        log = open(os.path.join(log_dir, "rfaRunner_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
+        return log
+    except (OSError, IOError):
+        # return -1 in case of exception
+        return -1
+
+
+def qaPrint(log, message):
+    """
+    Prints 'timestamp + message' to console and writes it to the log file
+    """
+    # current date and time as string + message. example: [Oct 25 01:52:33.000001] TC1 - Passed
+    log_message = getCurTime("[%b %d %H:%M:%S.%f]") + " " + message
+    # prints log_message
+    print log_message
+    # writes message to a log file
+    log.write(log_message + "\n")
+
+
+def getCurTime(date_time_format):
+    """
+    Returns current date_time as a string formatted according to date_time_format
+    """
+    date_time = datetime.now().strftime(date_time_format)
+    return date_time
+
+
+def getLocalEnv(prop_file):
+    try:
+        prop_set = {}
+        with open(os.path.join(os.getcwd(), "local.properties"), "r") as f:
+            for line in f:
+                key, value = line.strip("\n").split("=")
+                prop_set[key] = value
+        return prop_set
+    except IOError:
+        return -1
     
-    return open(full_path, 'a')
 
-
-def qaprint(log, message):    
-    msg = getCurTime().strftime('%Y-%m-%d %H:%M:%S') + ' - ' + message
-    print msg
-    log.write(msg + '\n')
-
-
-def getCurTime():
-    return datetime.datetime.utcnow()
+def getTestCases(trid):
+    try:
+        res = []
+        with open(os.path.join(os.getcwd(), str(trid) + ".txt"), "r") as f:
+            for line in f:
+                test_case = line.strip("\n").split("|")
+                res.append(test_case)
+        
+        return res
+    except IOError:
+        return -1
