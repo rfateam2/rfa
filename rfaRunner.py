4a4afb6a3931aa7ee06495f76cacaac73f7d6405
Ivan Korolevskiy <ivan.korolevskiy@emc.com>
2016-10-31 17:30:50 -0700

    RC with futction and verification

--- rfaRunner.py
+++ rfaRunner.py
@@ -6,23 +6,52 @@ Created on Oct 19, 2016
 @author: team 2
 @author: korvinca
 '''
-from rfaUtils import getLog,qaPrint
 
 import sys
+import os
+from rfaUtils import getLog, qaPrint, getTestCases, getLocalEnv
+
+
+trid = 0
+sc_name = ""
+local_dir = os.path.dirname(os.path.realpath(__file__))
+# get arguments
+for arg in sys.argv:
+    if "testrun" in arg.lower():
+        arg1 = arg.split("=")
+        trid = arg1[1]
+    else:
+        arg0 = arg.split(".")
+        sc_name = arg0[0]
+    # print sc_name, trid
+
+# get path to testrun_id file
+trid = "/".join([local_dir, trid]) + ".txt"
+
+# get path for log's directory
+loc_prop_file =str("/".join([local_dir, "local.properties"]))
+prop_dict = getLocalEnv(loc_prop_file)
+if prop_dict == -1:
+    print "Unable to crate the dictionary with local properties."
+    sys.exit("Unable to get local properties.")
+else:
+    log_dir_name = prop_dict['log_dir']
 
 # get the log file handle
-log = getLog()
-
+log = getLog(log_dir_name, sc_name)
 # exit if log creation failed
 if log == -1:
     sys.exit("Unable to create log file")
 
-message = "It is working, right?"
+# get test cases into the dictionary
+test_cases = getTestCases(trid)
+# exit if errors is with creating dictionary
+if test_cases == -1:
+    qaPrint(log, "Something wrong with TC's dictionary.")
+    sys.exit("Unable to get test cases.")
+else:
+    qaPrint(log, str(test_cases)) # optional
 
-# call qaPrint to print a message with timestamp and write it to the log file
-qaPrint(log, message)
-qaPrint(log, "Me like what me see")
-
-# close the log file if it open
+# close the log file if it is opened
 if not log.closed:
     log.close()
--- rfaUtils.py
+++ rfaUtils.py
@@ -9,23 +9,22 @@ from datetime import datetime
 import os
 
 
-def getTestCases(trid=None): # for test TODO delete None
+def getTestCases(trid):
     """
     Function will read corresponding id.txt file.
     Function will return that dictionary or -1 in case of error
     and will try to explain the reason.
     """
-    # trid = "42.txt" # for test TODO delete
-    message = ""
     try:
         handle = open(trid)
-    except IOError as message:
-        res_suite = -1
+    except IOError as err:
+        print "Failed to open file with error:", err
+        return -1
     else:
-        # can be accepted by argument or changed here
+        # can be accepted by argument or changed here to change value to int
         req_keys = ["rest_URL", "HTTP_method", "HTTP_RC_desired", "param_list"]
         res_suite = dict()
-        counts = 0
+        count = 0
         for line in handle:
             line = line.rstrip()
             if len(line) < 1 : continue
@@ -37,40 +36,44 @@ def getTestCases(trid=None): # for test TODO delete None
             one_test = dict(zip(req_keys, req_values))
             try:
                 # Change type of the value
+                # Better to do it here after merge dict
+                # make the function more universal
                 if "HTTP_RC_desired" in one_test:
                     # change value to int if the key is "HTTP_RC_desired"
-                    one_test["HTTP_RC_desired"] = int(one_test["HTTP_RC_desired"])
+                    try:
+                        one_test["HTTP_RC_desired"] = int(one_test["HTTP_RC_desired"])
+                    except ValueError as err:
+                        print "Error with changing type of the value:", err
+                        return -1
                 if "param_list" in one_test:
                     # change value to list if the key is "param_list"
                     one_test["param_list"] = one_test["param_list"].split(",")
-                counts += counts
-            except ValueError as message:
-                print "Error with changing type of the value:", message
-                break
+                count += 1
+            except ValueError as err:
+                print "Error with changing type of the value:", err
+                return -1
             # append sub-dictionaries with test to result
             res_suite[test_num] = one_test
-        if len(res_suite) != counts:
-            message = "Dictionary is broken, nothing to return."
-            res_suite = -1
-    # print res_suite, message # for test TODO delete
-    return res_suite, message
+        if len(res_suite) != count:
+            print "Tests Cases dictionary is broken, nothing to return."
+            return -1
+    return res_suite
 
-def getLocalEnv(prop_file=None): # for test TODO delete None
+def getLocalEnv(prop_file):
     """
     Read the content of the file, and return a dictionary,
     where 1 value of the file becomes key and second value becomes a value.
     """
-    # prop_file = "local.properties" # for test TODO delete
-    message = ""
     prop_set = dict()
     # list for integers
     list_for_int = ["debug_mode", 'test_server_port', 'test_db_port', 'verbose_log_mode']
     try:
         handle = open(prop_file)
-    except IOError as message:
-        prop_set = -1
+    except IOError as err:
+        print "Failed to open file with error:", err
+        return -1
     else:
-        counts = 0
+        count = 0
         for line in handle:
             line = line.rstrip()
             if len(line) < 1 : continue
@@ -78,26 +81,28 @@ def getLocalEnv(prop_file=None): # for test TODO delete None
             try:
                 if req_values[0] in list_for_int:
                     # change value to int if the key is from list list_for_int
-                    req_values[1] = int(req_values[1])
+                    try:
+                        req_values[1] = int(req_values[1])
+                    except ValueError as err:
+                        print "Filed to change type of the value in local.parametrs:", err
+                        return -1
                 # append pear in dictionary
                 prop_set[req_values[0]] = req_values[1]
-                counts += counts
-            except ValueError as message:
-                print "Error with changing type of the value:", message
-                break
-        if len(prop_set) != counts:
-            message = "Dictionary is broken, nothing to return."
-            prop_set = -1
-    # print prop_set, message # for test TODO delete
-    return prop_set, message
+                count += 1
+            except ValueError as err:
+                print "Error with changing type of the value:", err
+        if len(prop_set) != count:
+            print "Local Environment dictionary is broken, nothing to return."
+            return -1
+    return prop_set
 
-def getLog():
+def getLog(log_dir_name, sc_name):
     """
     Creates 'logs' directory, if it doesn't exist,
     creates or opens a log file in 'logs' directory.
     """
     # assign a current working directory + '/logs' to log_dir variable (platform independent)
-    log_dir = os.path.join(os.getcwd(), "logs")
+    log_dir = os.path.join(os.getcwd(), log_dir_name)
     # or --> script directory: log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
     # or --> user directory: log_dir = os.path.join(os.path.expanduser("~"), "logs")
 
@@ -106,7 +111,7 @@ def getLog():
         if not os.path.isdir(log_dir):
             os.makedirs(log_dir)
         # open log file with prefix and timestamp (platform independent) in Append mode
-        log = open(os.path.join(log_dir, "rfaRunner_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
+        log = open(os.path.join(log_dir, sc_name + "_" + getCurTime("%Y%m%d_%H-%M") + ".log"), "a")
         return log
     except (OSError, IOError):
         # return -1 in case of exception
@@ -131,6 +136,3 @@ def getCurTime(date_time_format):
     """
     date_time = datetime.now().strftime(date_time_format)
     return date_time
-
-if __name__ == '__main__':
-    getLocalEnv()
