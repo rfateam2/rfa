0a14eadb9acdd563fd734ec44fcbbae6d2002306
Nixer <nixer1978@yahoo.com>
2016-11-07 13:47:18 -0800

    code review#1 fixes

--- rfaRunner.py
+++ rfaRunner.py
@@ -3,27 +3,33 @@ Created on Oct 19, 2016
 
 
 @author: sashaalexander
-@author: team 2
+@author: nixer (team#2)
 '''
 from rfaUtils import *
 import sys
 
-trid = checkArgv()
-
-# get list of test_cases
-test_cases = getTestCases(trid)
+trid = checkArgv(sys.argv)
+if trid == -1:
+    sys.exit("Unable to process command-line arguments")
 
 loc_prop = getLocalEnv('local.properties')
 log_dir = loc_prop['log_dir']
 
 # get the log file handle
-log = getLog(log_dir, trid)
+log = getLog(log_dir, trid["--testrun"])
 
 # exit if log creation failed
 if log == -1:
     sys.exit("Unable to create log file")
 
 message = "It is working, right?"
+
+# get list of test_cases
+test_cases = getTestCases(trid["--testrun"])
+
+if test_cases[0] == -1:    
+    qaPrint(log, str(test_cases[1]))
+    sys.exit("Unable to get test cases from txt file: {}".format(test_cases[1]))
 
 # call qaPrint to print a message with timestamp and write it to the log file
 qaPrint(log, message)
--- rfaUtils.py
+++ rfaUtils.py
@@ -49,36 +49,39 @@ def getCurTime(date_time_format):
     date_time = datetime.now().strftime(date_time_format)
     return date_time
 
-def checkArgv():
-    # processing of command-line arguments
-    if len(sys.argv) != 2:
+def checkArgv(arg):
+    """
+    Processing of command-line arguments and return dictionary
+    """
+    trid = {}
+    if len(arg) < 2:
         sys.exit('one argument is required: --testrun=<trid>')
     else:
-        args = sys.argv[1].lower().split('=')
-        if args[0] == '--testrun' and args[1].isdigit() and len(args) > 1:
-            trid = args[1]
-            if int(trid) in range(10001):
-                pass
-            else:
-                sys.exit('test run id should be in range [0-10000]')
-        else:
-            sys.exit('rfaRunner.py --testrun=<trid>')
-        return trid
+        for i in range(1, len(arg)):
+            try:
+                i_arg = arg[i].lower().split('=')
+                if i_arg[0] == '--testrun' and int(i_arg[1]) not in range(10001):
+                    sys.exit('test run id should be in range [0-10000]')
+                trid[i_arg[0]] = i_arg[1]
+            except:
+                # return -1 in case of exception
+                return -1
+    return trid
 
 def getLocalEnv(loc_prop_file):
-    '''
+    """
     Reads the content of loc_prop_file to dictionary and returns it 
-    '''
+    """
     loc_prop = {}
     try:
         with open(loc_prop_file) as f:
             for line in f:
                 try:
-                   (key, val) = line.split('=')
-                   val = val.strip()
-                   if val.isdigit(): # checking if value is digit -> convert to int
-                       val = int(val)
-                   loc_prop[key] = val
+                    (key, val) = line.split('=')
+                    val = val.strip()
+                    if val.isdigit(): # checking if value is digit -> convert to int
+                        val = int(val)
+                    loc_prop[key] = val
                 except ValueError as err:
                     print "Local properties file has wrong format: ", err 
                     return -1
@@ -88,12 +91,14 @@ def getLocalEnv(loc_prop_file):
         return -1
 
 def getTestCases(trid): 
-    '''
+    """
     Reads the content of test_run_file to dictionary of dictionaries and returns it 
-    '''   
+    """   
     keys = ['rest_URL', 'HTTP_method', 'HTTP_RC_desired', 'param_list'] # key list
     test_cases = {}
     try:
+        if os.stat(trid+".txt").st_size == 0: # Check for empty file
+            return (-1, "File {} is empty".format(trid+".txt"))
         with open(trid+".txt") as f:
             tc_list = f.readlines() # reads each line of test_run_file in list
         for i in range(0, len(tc_list)): 
@@ -104,7 +109,7 @@ def getTestCases(trid):
             http_rc = int(dictionary['HTTP_RC_desired']) # convert HTTP_RC_desired value to int
             dictionary['HTTP_RC_desired'] = http_rc # replace value of HTTP_RC_desired by new int value
             test_cases[int(tc[0])] = dictionary # append final dictionary to test_cases dictionary      
-        return test_cases
-    except (OSError, IOError):
+        return (1, test_cases)
+    except (OSError, IOError) as err:
         # return -1 in case of exception
-        return -1
+        return (-1, err)
