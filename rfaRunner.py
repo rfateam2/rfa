feeb5c5c2bfb3c1151de3f3cfddeccd6331f0f4d
Nixer <nixer1978@yahoo.com>
2016-11-07 15:07:47 -0800

    code review#2 fixes

--- rfaRunner.py
+++ rfaRunner.py
@@ -5,7 +5,7 @@ Created on Oct 19, 2016
 @author: sashaalexander
 @author: nixer (team#2)
 '''
-from rfaUtils import *
+from rfaUtils import getLog, qaPrint, getTestCases, getLocalEnv, checkArgv 
 import sys
 
 trid = checkArgv(sys.argv)
