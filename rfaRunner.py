5f71e51a2f59c238ceb06575c374d240ec76c4fb
Ivan Korolevskiy <ivan.korolevskiy@emc.com>
2016-12-05 13:12:16 -0800

    Assignment 04

--- rfaRunner.py
+++ rfaRunner.py
@@ -1,52 +1,41 @@
 '''
 Created on Oct 19, 2016
-
-
 @author: sashaalexander
-@author: team 2
+@author: team 9
 '''
-
 import sys
-import os
-from rfaUtils import getLog, qaPrint, getTestCases, getLocalEnv, checkArgv
+from __builtin__ import str
 
-local_dir = os.path.dirname(os.path.realpath(__file__))
+from rfaUtils import getLog, qaPrint, getLocalEnv, getTestCases, closeLog, getArguments
 
-# Pull arguments from cli to dictionary,split by "="
-arg_dict = checkArgv(sys.argv)
-if arg_dict == -1:
-    sys.exit("ERROR: Unable to crate the dictionary with arguments.")
+# process command line arguments
+arguments = getArguments(sys.argv)
 
-#print arg_dict # Print output for check
-
-# get path to testrun_id file
-trid = "/".join([local_dir, arg_dict["--testrun"]]) + ".txt"
-
-# get path for local.properties file
-loc_prop_file = str("/".join([local_dir, "local.properties"]))
-
-# get local.properties environment in dictionary
-prop_dict = getLocalEnv(loc_prop_file)
-if prop_dict == -1:
-    sys.exit("ERROR: Unable to crate the dictionary with local properties.")
-
-#print prop_dict # Print output for check
-
+# read properties
+localProperties = getLocalEnv('local.properties')
+if localProperties == -1:
+    sys.exit('[ERROR]Could not read properties')
+if 'log_dir' not in localProperties.keys():
+    sys.exit("[ERROR]log_dir property is missing")
 
 # get the log file handle
-log = getLog(prop_dict["log_dir"], arg_dict["file_name"])
+log = getLog(arguments['testName'], localProperties['log_dir'])
+
 # exit if log creation failed
 if log == -1:
-    sys.exit("ERROR: Unable to create log file")
+    sys.exit("[ERROR]Could not create log file")
+qaPrint(log, "[INFO]Test suite starts")
 
-# get test cases into the dictionary
-test_cases = getTestCases(trid)
-if test_cases[0] == -1:
-    qaPrint(log, test_cases[1])
-    sys.exit("ERROR: Unable to get test cases from input file %s.txt" % arg_dict["--testrun"])
+# read test cases
+test_cases = getTestCases(arguments['trid'], log)
 
-qaPrint(log, "INFO:" + str(test_cases))
+if test_cases == -1:
+    qaPrint(log, '[ERROR]Could not read test cases')
+    closeLog(log)
+    sys.exit()
+else:
+    qaPrint(log, '[INFO]Got test cases. Testrun id is ' + str(arguments['trid']))
+    for key, value in test_cases.iteritems():
+        qaPrint(log, '[INFO]Test case #' + key + str(value))
 
-# close the log file if it is opened
-if not log.closed:
-    log.close()
+closeLog(log)
